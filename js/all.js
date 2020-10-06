ac1f723e5f1f5343a4aae43e6f4c5b0b78520985
Liao <s1031581@gm.pu.edu.tw>
2020-10-06 14:06:13 +0800

    edit bmistatus

--- js/all.js
+++ js/all.js
@@ -42,46 +42,40 @@ function update(item){
 	let year = date.getFullYear();
 	let month = date.getMonth()+1;
     let day = date.getDate();
+    var bmiStatus = {
+        superthin: {
+            class: 'superthin',
+        },
+        verythin: {
+            class: 'verythin',
+        },
+        thin: {
+            class: 'thin',
+        },
+        normal:{
+            class: 'normal'
+        },
+        fat:{
+            class: 'fat'
+        },
+        veryfat:{
+            class: 'veryfat'
+        },
+        superfat:{
+            class: 'superfat'
+        }
+        };
     for(let i = 0; i < itemLen ; i++){
+        let bmi = BMI(data[i]);
+        let content = data[i].body;
         let height = item[i].heightVal / 100;
         let weight = item[i].weightVal;
-        let bmi = (weight / Math.pow(height,2)).toFixed(2);
-        let result = "";
-        switch(true){
-            case bmi <=15 :{
-                result = "非常嚴重的體重不足";
-                break;
-            }
-            case bmi >15 && bmi <=16 :{
-                result = '嚴重體重不足';
-                break;
-            }
-            case bmi >16 && bmi <=18.5 :{
-                result = '體重過輕';
-                break;
-            }
-            case bmi >18.5 && bmi <=25:{
-                result = '體重正常';
-                break;
-            }
-            case bmi >25 && bmi <=30:{
-                result = '體重過重';
-                break;
-            }
-            case bmi >30 && bmi <=35:{
-                result = '中等肥胖';
-                break;
-            }
-            case bmi >35 && bmi <=40:{
-                result = '嚴重肥胖';
-                break;
-            }
-        }
-        console.log(result);
+        let bmiVal = (weight / Math.pow(height,2)).toFixed(2);
         str+= `    
                 <div class="d-flex justify-content-center align-items-center py-3">
-                    <div class="text-white mr-3">${result}</div>
-                    <div class="text-white mr-3">BMI : ${bmi}</div>
+                    <div class="circle mr-3 ${bmiStatus[content].class}"></div> 
+                    <div class="text-white mr-3">${bmi}</div>
+                    <div class="text-white mr-3">BMI :${bmiVal} </div>
                     <div class="text-white mr-3">Height : ${item[i].heightVal} cm</div>
                     <div class="text-white mr-3">Weight : ${item[i].weightVal} kg</div>
                     <div class="text-white mr-3">${year+'/'+month+'/'+day}</div>
@@ -90,6 +84,41 @@ function update(item){
         `
     }
     list.innerHTML = str;
+}
+function BMI(item){
+    let height = item.heightVal / 100;
+    let weight = item.weightVal;
+    let bmi = (weight / Math.pow(height,2)).toFixed(2);
+    switch(true){
+        case bmi <=15 :{
+            item.body = 'superthin';
+            return  "非常嚴重的體重不足";
+        }
+        case bmi >15 && bmi <=16 :{
+            item.body = 'verythin';
+            return  '嚴重體重不足';
+        }
+        case bmi >16 && bmi <=18.5 :{
+            item.body = 'thin';
+            return  '體重過輕';
+        }
+        case bmi >18.5 && bmi <=25:{
+            item.body = 'normal';
+            return  '體重正常';
+        }
+        case bmi >25 && bmi <=30:{
+            item.body = 'fat';
+            return  '體重過重';
+        }
+        case bmi >30 && bmi <=35:{
+            item.body = 'veryfat';
+            return  '中等肥胖';
+        }
+        case bmi >35 && bmi <=40:{
+            item.body = 'superfat';
+            return  '嚴重肥胖';
+        }
+    }
 }
 function deleteData(e){
     let index = e.target.dataset.index;
